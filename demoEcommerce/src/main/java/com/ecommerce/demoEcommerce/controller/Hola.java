ed6d6d99b351d36968a5d745eed61a051890ebc3
Acv3-1 <multiwafle@gmail.com>
2025-03-23 19:49:20 -0500

    cambios realizados

---
+++ demoEcommerce/src/main/java/com/ecommerce/demoEcommerce/controller/Hola.java
@@ -0,0 +1,18 @@
+package com.ecommerce.demoEcommerce.controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class Hola {
+    
+    @GetMapping("/aa")
+    public String hola(Model model){
+        model.addAttribute("nombreUsuario", "Juan");
+        return "prueba";
+    }
+
+}
