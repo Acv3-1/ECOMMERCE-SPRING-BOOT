68df5b905dbaf48603029a49d10c6872f54ff6b6
Camilo Soler <caansobu2@gmail.com>
2025-03-30 18:11:59 -0500

    cambios

--- demoEcommerce/src/main/java/com/ecommerce/demoEcommerce/controller/RegistroController.java
+++ demoEcommerce/src/main/java/com/ecommerce/demoEcommerce/controller/RegistroController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ecommerce.demoEcommerce.models.Cliente;
 import com.ecommerce.demoEcommerce.repository.ClienteRepository;
 //import org.springframework.web.servlet.view.RedirectView;
 
-@RestController
+@Controller
 @RequestMapping("/registro")
 public class RegistroController {
 
@@ -18,12 +20,14 @@ public class RegistroController {
     private ClienteRepository clienteRepository;
 
     @PostMapping
-    public String registrarCliente(@RequestParam String correo, @RequestParam String password, @RequestParam String confirmPassword) {
+    public String registrarCliente(@RequestParam int cedula, @RequestParam String nombre,@RequestParam String correo, @RequestParam String password, @RequestParam String confirmPassword) {
         if (!password.equals(confirmPassword)) {
-            return "Las contraseñas no coinciden.";
+            return "redirect:/registro?error=Passwords do not match";
         }
 
         Cliente nuevoCliente = new Cliente();
+        nuevoCliente.setCcCliente(cedula);
+        nuevoCliente.setNombre(nombre); // Cambia esto según tu lógica
         nuevoCliente.setCorreo(correo);
         nuevoCliente.setPassword(password); // Recuerda usar encriptación como BCrypt
         
